f96bdf94f2a3e40d3c5f5c78ad81a1a0e79983ce
ZUZ <megazuz@gmail.com>
2024-06-16 01:45:14 +0300

    Lesson 09 hard finished

--- hard.js
+++ hard.js
@@ -1,4 +1,5 @@
 'use strict';
+
 // lesson01
 
 // let num = 266219;
@@ -18,6 +19,7 @@
 // console.log('Z в кубе = ' + Z3);
 // console.log('проверка: ' + (2 * 6 * 6 * 2 * 1 * 9) * (2 * 6 * 6 * 2 * 1 * 9) * (2 * 6 * 6 * 2 * 1 * 9));
 // console.log('первые две цифры числа "Z в кубе": ' + Z3.toString().substring(0, 2));
+
 
 // lesson02
 
@@ -47,6 +49,7 @@
 // const langWeek = [enWeek, ruWeek];
 // lang == 'en' ? langWeek[0].forEach((day) => console.log(day)) : langWeek[1].forEach((day) => console.log(day));
 
+
 // lesson03
 
 // // 'Артём' = 'директор'
@@ -54,6 +57,7 @@
 // // остальные = 'студент'
 // let namePerson = 'Артём';
 // namePerson == 'Артём' ? console.log('директор') : namePerson == 'Александр' ? console.log('преподаватель') : console.log('студент');
+
 
 // lesson04
 
@@ -70,6 +74,7 @@
 // }
 
 // console.log('"' + trimText30(someText) + '"');
+
 
 // lesson05
 
@@ -99,30 +104,89 @@
 //     };
 // };
 
+
 // lesson06 находится в отдельном репозитории
+
 
 // lesson07
 
 // let body = document.body;
 // console.log(body);
 
-const week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
-// const startText = ['<h3>Календарь на текущую неделю</h3>', '<ul>'];
-// const endText = ['</ul>'];
-const date = new Date();
-const day = date.getDay() - 1;
-// console.log(day);
+// const week = ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday'];
+// // const startText = ['<h3>Календарь на текущую неделю</h3>', '<ul>'];
+// // const endText = ['</ul>'];
+// const date = new Date();
+// const day = date.getDay() - 1;
+// // console.log(day);
 
-week.forEach(function (item, index) {
-    let style = '';
-    if (index == day) {
-        style += 'font-weight: bold;'
-    };
-    if ((index == 5) || (index == 6)) {
-        style += 'font-style: italic;';
-    }
-    console.log('%c' + item, style);
-});
+// week.forEach(function (item, index) {
+//     let style = '';
+//     if (index == day) {
+//         style += 'font-weight: bold;'
+//     };
+//     if ((index == 5) || (index == 6)) {
+//         style += 'font-style: italic;';
+//     }
+//     console.log('%c' + item, style);
+// });
 
 // body.textContent = startText.join('\n') + calendar.join('\n') + endText.join('\n');
 // console.log(body);
+
+
+// lesson09
+
+// 1) Выведите на страницу текущую дату и время в 2-х форматах: 
+// а) '04.02.2020 - 21:05:33'
+// б) 'Сегодня Вторник, 4 февраля 2020 года, 21 час 5 минут 33 секунды' 
+// 2) Для вывода в формате (а) напишите функцию, которая будет добавлять 0 перед
+// значениями которые состоят из одной цифры (из 9:5:3 1.6.2019 сделает 09:05:03 01.06.2019)
+// 3) Для вывода в формате (б) напишите функцию, которая будет менять склонение
+// слов в зависимости от числа, "час, часов, часа"
+// 4) С помощью функции setInterval, реализуйте обновление даты и времени каждую секунду
+
+const node1 = document.createElement('h2');
+const nodeText1 = document.createTextNode('Текущее время:');
+node1.appendChild(nodeText1);
+document.body.appendChild(node1);
+
+const node2 = document.createElement('h3');
+const nodeText2 = document.createTextNode('01.01.1970 - 00:00:00');
+node2.appendChild(nodeText2);
+node2.id = 'date-text-1';
+document.body.appendChild(node2);
+
+const node3 = document.createElement('h3');
+const nodeText3 = document.createTextNode('Сегодня Четверг, 1 января 1970 года, 00 часов 00 минут 00 секунд');
+node3.appendChild(nodeText3);
+node3.id = 'date-text-2';
+document.body.appendChild(node3);
+
+const updateDate = (id1, id2) => {
+    const dateText1 = document.getElementById(id1);
+    const dateText2 = document.getElementById(id2);
+    const updateTime = 1000; // 1000 милисекунд = 1 секунда
+    const ruWeek = ['Воскресенье', 'Понедельник', 'Вторник', 'Среда', 'Четверг', 'Пятница', 'Суббота'];
+    const ruMonth = [' января ', ' февраля ', ' марта ', ' апреля ', ' мая ', ' июня ', ' июля ', ' августа ', ' сентября ', ' октября ', ' ноября ', ' декабря '];
+    let dateSeconds = Date.now();
+    let newDate = new Date(dateSeconds);
+    const numWord = (value, words) => {
+        const ost100 = value % 100;
+        if (ost100 >= 5 && (ost100 <= 20)) return value + words[2];
+        const ost10 = ost100 % 10;
+        if (ost10 == 1) return value + words[0];
+        if (ost10 > 1 && ost10 < 5) return value + words[1];
+        return value + words[2];
+    };
+    const update = () => {
+        const time = numWord(newDate.getHours(), [' час ', ' часа ', ' часов ']) + numWord(newDate.getMinutes(), [' минута ', ' минуты ', ' минут ']) + numWord(newDate.getSeconds(), [' секунда', ' секунды', ' секунд']);
+        dateText2.textContent = 'Сегодня ' + ruWeek[newDate.getDay()] + ', ' + newDate.getDate() + ruMonth[newDate.getMonth()] + newDate.getFullYear() + ' года, ' + time;
+        dateText1.textContent = newDate.toLocaleString().replace(', ', ' - ');
+        dateSeconds += updateTime;
+        newDate = new Date(dateSeconds);
+    }
+    update();
+    setInterval(update, updateTime);
+}
+updateDate('date-text-1', 'date-text-2');
